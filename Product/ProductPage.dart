4488b4e1faad783054a57eea41cfcb61024a341c
Athulya Anil <86157424+Athulya2k02@users.noreply.github.com>
2024-12-24 15:37:34 +0530

    ProductPage.dart

--- Product/ProductPage.dart
+++ Product/ProductPage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shopme/screens/Product/ProductService.dart';
 import 'package:shopme/screens/Product/Stock.dart';
 import 'package:shopme/screens/Product/ViewProduct.dart';
-import 'package:shopme/screens/home.dart';  
+import 'package:shopme/screens/home.dart';
 
 void main() => runApp(const ProductPageApp());
 
@@ -35,13 +35,7 @@ class _ProductPage1State extends State<ProductPage1> {
     const MenuCardPage(),
     const Center(
       child: Text(
-        'Order History Page',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      ),
-    ),
-    const Center(
-      child: Text(
-        'Add Product Page',
+        'Home Page',
         style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
       ),
     ),
@@ -66,14 +60,14 @@ class _ProductPage1State extends State<ProductPage1> {
           ),
         ),
         centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 2, 0, 16),
+        backgroundColor: const Color.fromARGB(255, 2, 0, 16),
       ),
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              Color.fromARGB(255, 199, 192, 237),
               Color.fromARGB(255, 255, 255, 255),
+              Color.fromARGB(255, 8, 1, 52)
             ],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
@@ -84,7 +78,7 @@ class _ProductPage1State extends State<ProductPage1> {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        backgroundColor: Color.fromARGB(255, 2, 0, 16),
+        backgroundColor: const Color.fromARGB(255, 2, 0, 16),
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white70,
         items: const [
@@ -107,59 +101,66 @@ class MenuCardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 50.0,
-        mainAxisSpacing: 50.0,
-        children: [
-          _buildMenuCard(
-            icon: Icons.visibility,
-            title: 'View Products',
-            color: const Color.fromARGB(255, 2, 0, 16),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ViewProductPage()),
-              );
-            },
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        
+        int crossAxisCount = constraints.maxWidth < 600 ? 2 : 4;
+
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: GridView.count(
+            crossAxisCount: crossAxisCount,
+            crossAxisSpacing: 16.0,
+            mainAxisSpacing: 16.0,
+            children: [
+              _buildMenuCard(
+                icon: Icons.visibility,
+                title: 'View Products',
+                color: const Color.fromARGB(255, 2, 0, 16),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ViewProductPage()),
+                  );
+                },
+              ),
+              _buildMenuCard(
+                icon: Icons.add_circle_outline,
+                title: 'Add Products',
+                color: const Color.fromARGB(255, 2, 0, 16),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProductPage()),
+                  );
+                },
+              ),
+              _buildMenuCard(
+                icon: Icons.shopping_basket,
+                title: 'Products',
+                color: const Color.fromARGB(255, 2, 0, 16),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const StockPage()),
+                  );
+                },
+              ),
+              _buildMenuCard(
+                icon: Icons.settings_accessibility,
+                title: 'Settings',
+                color: const Color.fromARGB(255, 2, 0, 16),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+              ),
+            ],
           ),
-          _buildMenuCard(
-            icon: Icons.add_circle_outline,
-            title: 'Add Products',
-            color: const Color.fromARGB(255, 2, 0, 16),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProductPage()),
-              );
-            },
-          ),
-          _buildMenuCard(
-            icon: Icons.shopping_basket,
-            title: 'Products',
-            color: const Color.fromARGB(255, 2, 0, 16),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const StockPage()),
-              );
-            },
-          ),
-          _buildMenuCard(
-            icon: Icons.settings_accessibility,
-            title: 'Settings',
-            color: const Color.fromARGB(255, 2, 0, 16),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
-              );
-            },
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 
@@ -174,15 +175,15 @@ class MenuCardPage extends StatelessWidget {
       child: Card(
         color: color,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(6.0),
+          borderRadius: BorderRadius.circular(8.0),
         ),
-        elevation: 8.0,
+        elevation: 6.0,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(
               icon,
-              size: 24.0,
+              size: 32.0,
               color: Colors.white,
             ),
             const SizedBox(height: 10),
